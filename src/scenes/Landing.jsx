22efeeda2253b55f28ec4a0b785d447b4a18cf30
Fatima Rahimi <rahimifatima432@gmail.com>
2025-08-24 10:11:34 +0430

    add tow p

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -20,6 +20,27 @@ const Landing = ({setSelectedPage}) => {
                  <img src="../assets/profile-image.png" alt="profile" className="hover:filter hover:saturate-200 
                 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"/>
             )}
+            {/* MAIN SECTION*/}
+            <div className="z-30 basis-2/5 mt-12 md:mt-32">
+              {/* Headers */}
+              <motion.div initial="hidden" whileInView="visible"
+              viewport={{once: true, amount: 0.5}}
+              transition={{ duration: 0.5}}
+              variants={{
+                hidden: {opacity: 0, x: -50},
+                visible: { opacity: 1, x: 0}
+              }}
+              >
+               <p className="text-6xl font-palyfair z-10 text-center md:text-start">
+                 Jane {""}
+                <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+                before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]">Esper</span>
+               </p>
+               <p>Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum viverra malesuada sem ac 
+                faucibus dolor. Sagittis scelerisque.
+               </p>
+              </motion.div>
+            </div>
 
             </div>
         </section>
